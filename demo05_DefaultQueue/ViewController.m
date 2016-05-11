06fb1eb72bbf374cdb83419b140f911a996574de
LuoShimei <Luo_Shimei@163.com>
2016-05-11 18:45:42 +0800

    全局队列异步，主队列异步
    主队列同步会造成死锁，所以不用

--- demo05_DefaultQueue/ViewController.m
+++ demo05_DefaultQueue/ViewController.m
@@ -23,17 +23,60 @@
 
 /** 全局队列异步 */
 - (IBAction)globalQueueAsync:(id)sender {
+    //获取系统创建的主队列，主队列的生命周期跟进程的一致
+    dispatch_queue_t globalQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    
+    dispatch_async(globalQueue, ^{
+        for (int i = 0; i < 10; i++) {
+            NSLog(@"全局队列异步：i = %d,current thread:%@",i,[NSThread currentThread]);
+            [NSThread sleepForTimeInterval:0.5];
+        }
+    });
+    
+    dispatch_async(globalQueue, ^{
+        for (int j = 0; j < 10; j++) {
+            NSLog(@"全局队列异步：j = %d,current thread:%@",j,[NSThread currentThread]);
+            [NSThread sleepForTimeInterval:0.5];
+        }
+    });
+    
     
 }
 
 /** 主队列异步 */
 - (IBAction)mainQueueAync:(id)sender {
+    //获取主队列
+    dispatch_queue_t mainQueue = dispatch_get_main_queue();
     
+    dispatch_async(mainQueue, ^{
+        for (int i = 0; i < 10; i++) {
+            NSLog(@"主队列异步：i = %d,current thread:%@",i,[NSThread currentThread]);
+            [NSThread sleepForTimeInterval:0.5];
+        }
+    });
+    
+    dispatch_async(mainQueue, ^{
+        for (int j = 0; j < 10; j++) {
+            NSLog(@"主队列异步：j = %d,current thread:%@",j,[NSThread currentThread]);
+            [NSThread sleepForTimeInterval:0.5];
+        }
+    });
 }
 
-/** 主队列同步 */
+/** 主队列同步(会造成死锁，不会使用的) */
+//dispatch_sync此方法 要求主队列立即执行block中语句
+//主线程 你要排队,因为主线程是串行的，必须等你之前的任务做完 才能帮你
+//同步 要求线程立刻开始做， 异步 有时间就执行
 - (IBAction)mainQueueSync:(id)sender {
+    //获取主队列
+    dispatch_queue_t mainQueue = dispatch_get_main_queue();
     
+    dispatch_sync(mainQueue, ^{
+        for (int i = 0; i < 10; i++) {
+            NSLog(@"主队列异步：i = %d,current thread:%@",i,[NSThread currentThread]);
+            [NSThread sleepForTimeInterval:0.5];
+        }
+    });
 }
 
 
